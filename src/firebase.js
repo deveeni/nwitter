78f26313470dc548f176105161c97d9a310d16b4
Hyobin-Im <mandarino.viv.21@gmail.com>
2021-02-23 00:10:24 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCF6sxOIciZ9czkd5VKeV6ZiLG0IIKZ778",
+    authDomain: "nwitter-1e8ae.firebaseapp.com",
+    projectId: "nwitter-1e8ae",
+    storageBucket: "nwitter-1e8ae.appspot.com",
+    messagingSenderId: "1018281362693",
+    appId: "1:1018281362693:web:44c17de92fbe14cc47a36c"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
