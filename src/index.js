3f28c5aad1736c45b487a00e3e419b7a194e8f14
Hyobin-Im <mandarino.viv.21@gmail.com>
2021-02-24 18:19:45 +0900

    add firbase Auth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import firebase from './firebase';
-
-console.log("firebase is import");
 
 ReactDOM.render(
   <React.StrictMode>
