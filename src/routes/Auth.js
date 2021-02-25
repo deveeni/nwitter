e1d5b12cb17297550149a3065740ab353be1cbce
Hyobin-Im <mandarino.viv.21@gmail.com>
2021-02-25 22:39:20 +0900

    update Auth.js

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,52 @@
-import react from "react";
+import react, { useState } from "react";
 
-export default () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+
+        if(name === "email"){
+            setEmail(value);
+        }else if(name === "password"){
+            setPassword(value);
+        }
+    };
+
+    const onSubmit = (event) => {
+       event.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    required
+                    value={password}
+                    onChange={onChange}
+                />
+
+                <input type="submit" value="Log In" />
+
+                <div>
+                    <button>Continue with google</button>
+                    <button>Continue with Github</button>
+                </div>
+            </form>
+        </div>
+    )
+};
+export default Auth;
